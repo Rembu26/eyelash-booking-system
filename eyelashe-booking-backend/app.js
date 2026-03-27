1ef4999ceb93127583ca51738ac359d5d4f5e38c
Rembu <s227320840@mandela.ac.za>
2026-03-27 03:15:07 +0200

    Linking Login with BE

--- eyelashe-booking-backend/app.js
+++ eyelashe-booking-backend/app.js
@@ -1,6 +1,11 @@
 // Import required packages
 const express = require('express');
 const mongoose = require('mongoose');
+const authMiddleware = require('./middleware/authMiddleware');
+const cors = require('cors');
+
+require('dotenv').config();
+
 
 // Import routes
 const authRoutes = require('./routes/authRoutes');
@@ -8,11 +13,21 @@ const authRoutes = require('./routes/authRoutes');
 // Create Express app
 const app = express();
 
+// Enable CORS
+app.use(cors({
+    origin: 'http://localhost:3001', // Frontend URL
+    credentials: true
+}));
+
+app.get('/api/protected', authMiddleware, (req, res) => {
+    res.json({"You are authenticated": req.user});
+});
+
 // Middleware (to read JSON data)
 app.use(express.json());
 
 // MongoDB connection string
-const uri = "mongodb+srv://rembuluwanim4_db_user:tv943ULjxjmvzaAY@eyelashes-db.f1za9xs.mongodb.net/test?retryWrites=true&w=majority";
+const uri = "mongodb+srv://rembuluwanim4_db_user:RMGhLWZfOjVJQC4W@eyelashes-db.f1za9xs.mongodb.net/test?retryWrites=true&w=majority";
 
 // Connect to MongoDB using Mongoose
 mongoose.connect(uri, {
--- eyelashe-booking-backend/controllers/controller.js
+++ eyelashe-booking-backend/controllers/controller.js
@@ -20,28 +20,43 @@ exports.register = async (req, res) => {
     }
 };
 
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
 
 // LOGIN FUNCTION
 exports.login = async (req, res) => {
+    
     try {
         const { username, password } = req.body;
 
+        // Find user by username
         const user = await User.findOne({ username });
 
         if (!user) {
             return res.status(400).json({ message: "User not found" });
         }
-
+        //Check password
         const isMatch = await user.comparePassword(password);
 
         if (!isMatch) {
             return res.status(400).json({ message: "Invalid password" });
         }
 
+        // Create JWT token
+        const token = jwt.sign(
+            { id: user._id, 
+                username: user.username,
+            role: user.role },
+            process.env.JWT_SECRET,// Secret key from .env file
+            { expiresIn: '1h' }
+        );
+        //Send response with token
         res.json({
             message: "Login successful",
+            token,
             role: user.role
         });
+
 
     } catch (err) {
         res.status(500).json({ error: err.message });
